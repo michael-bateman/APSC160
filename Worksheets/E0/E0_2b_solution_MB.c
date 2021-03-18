801455d7f695838b69b58365d0d753d5ddeb710e
Michael Bateman <michaelbateman@michaelbateman.ca>
2021-03-18 15:55:42 -0700

    Add problems #2

---
+++ Worksheets/E0/E0_2b_solution_MB.c
@@ -0,0 +1,55 @@
+/*
+    Author: Michael Bateman
+    Email: mikeb8ma@student.ubc.ca
+    Student Number: 14805246
+    Lab Section: L2M
+    Today's Date: March 14, 2021
+ 
+    Purpose: simple loop with sum
+    Input:
+        none
+    Output:
+        i - the amount of times the loop is run
+        sum - the sum of i's
+*/
+
+#include <stdio.h>
+
+//Define
+#define NUM_ROWS 2
+#define NUMCOLS 5
+#define TRUE 1
+#define FALSE 0
+
+//Function protype
+int search(int value, int array[NUM_ROWS][NUMCOLS]);
+
+int main(void) {
+    
+    //Test array
+    int array[NUM_ROWS][NUMCOLS] = {
+        {2,4,6,8,10},
+        {12,14,16,18,20}
+    };
+    printf("%d\n", search(2, array));
+    return 0;
+}
+
+//Function search
+int search(int value, int array[NUM_ROWS][NUMCOLS]) {
+    int index_row = 0;
+    int index_col = 0;
+    
+    // Loop to check 2D array
+    while (index_row < NUM_ROWS) {
+        while (index_col < NUMCOLS) {
+            if (array[index_row][index_col] == value) {
+                return TRUE;
+            }
+            index_col++;
+        }
+        index_row++;
+    }
+    
+    return FALSE;
+}
